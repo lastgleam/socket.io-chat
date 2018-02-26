8b6d598647355a2f1d565ee494cf4268568b6943
lastgleam <lastgleam@gmail.com>
2018-02-26 20:00:36 +0900

    update cofig for heroku

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const options = {
     pingInterval: 3000
 };
 const io = require('socket.io')(server, options);
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
 
 server.listen(30000, () => {
     console.log('listening on *:30000');
