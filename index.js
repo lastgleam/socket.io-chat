21b62a7f101379cab01292179ee34ca433ae46ed
lastgleam <lastgleam@gmail.com>
2018-02-26 13:31:39 +0900

    update join & leave room

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const options = {
+    pingTimeout: 60000,
+    pingInterval: 3000
+};
+const io = require('socket.io')(server, options);
 
-http.listen(30000, () => {
+server.listen(30000, () => {
     console.log('listening on *:30000');
 });
 
@@ -10,29 +15,43 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+const users = {};
 const rooms = {};
 
 io.on('connection', socket => {
     const socket_id = socket.id;
+    users[socket_id] = {};
     socket.emit('roomlist', rooms);
 
     console.log(socket.id + ' has connected');
     io.to(socket.id).emit('getId', socket.id);
+    console.log('users', users);
+    console.log('rooms', rooms);
 
     socket.on('disconnect', () => {
         socket.emit('user disconnected');
+        let user = users[socket.id];
+        if (rooms[user.roomName] && rooms[user.roomName].includes(socket.id)) {
+            let user = users[socket_id];
+            let index = rooms[user.roomName].indexOf(socket.id);
+            delete user.roomName;
+            rooms[user.roomName].splice(index, 1);
+        }
         console.log(socket.id + ' has disconnected');
+        console.log('users', users);
+        console.log('rooms', rooms);
     });
 
     socket.on('join', (roomName, user) => {
         socket.join(roomName);
         if (rooms[roomName]) {
-            if(!rooms[roomName].includes(user.socket_id)){
+            if (!rooms[roomName].includes(user.socket_id)) {
                 rooms[roomName].push(user.socket_id);
             }
         } else {
             rooms[roomName] = [user.socket_id];
         }
+        users[user.socket_id] = user;
         socket.in(roomName).emit(socket_id, roomName);
         const info = {
             'status': 'join',
@@ -42,26 +61,33 @@ io.on('connection', socket => {
         console.log(socket_id + ' has joined to ' + roomName);
         socket.in(roomName).emit('info', info);
         socket.emit('roomlist', rooms);
-        console.log('rooms', rooms)
+        console.log('users', users);
+        console.log('rooms', rooms);
     });
 
     socket.on('leave', (roomName, user) => {
         socket.leave(roomName);
-        const index = rooms[roomName].indexOf(user.socket_id);
-        rooms[roomName].splice(index, 1);
-        if (rooms[roomName].length < 1) {
-            delete rooms[roomName];
-            console.log(rooms);
+        if(rooms[roomName] && rooms[roomName].includes(user.socket_id)){
+            let index = rooms[roomName].indexOf(user.socket_id);
+            rooms[roomName].splice(index, 1);
+            delete users[user.socket_id].roomName;
+            if (rooms[roomName].length < 1) {
+                delete rooms[roomName];
+                console.log(rooms);
+            }
+            let info = {
+                'status': 'leave',
+                'socket_id': socket_id,
+                'roomName': roomName
+            };
+            console.log(user.socket_id + ' has left from ' + roomName);
+            socket.in(roomName).emit('info', info);
+            socket.emit('roomlist', rooms);
+            console.log('users', users);
+            console.log('rooms', rooms);
+        }else{
+            console.log(socket.id + "is not entered to any rooms.");
         }
-        const info = {
-            'status': 'leave',
-            'socket_id': socket_id,
-            'roomName': roomName
-        };
-        console.log(user.socket_id + ' has left from ' + roomName);
-        socket.in(roomName).emit('info', info);
-        socket.emit('roomlist', rooms);
-        console.log('rooms',rooms)
     });
 
     socket.on('rooms', () => {
